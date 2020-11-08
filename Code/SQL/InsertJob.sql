515ba4a4d0f4dc1bb69163799631b0a1e9247e3a
forchain <tony.goldenweek@gmail.com>
2020-11-08 22:18:45 +0800

    fix import students

--- Code/SQL/InsertJob.sql
+++ Code/SQL/InsertJob.sql
@@ -2,6 +2,6 @@ INSERT INTO Job (Title, Display, [Order],IsStudent)
 SELECT  DISTINCT JobTitle
        ,JobTitle
        ,IIf(IsStudent, 2, 1)
-       ,InStr(JobTitle,"Stduent") <> 0 or IsStudent
+       ,IsStudent
 FROM ImportAuthor
 WHERE JobTitle not IN ( SELECT DISTINCT Title FROM Job )  
--- Code/SQL/MakeImportAuthorStudent.sql
+++ Code/SQL/MakeImportAuthorStudent.sql
@@ -1,9 +1,9 @@
 SELECT  DISTINCT IIF(IsNull([ID]),GetTempCode(DepartmentID,JobTitle,FullName),[ID]) AS Code 
-       ,[First Name] & " " & [Last name]                                                      AS FullName 
-       ,FixTitle([Position])                                                            AS JobTitle 
-       ,ExtractOutDepID([Department])                                                AS DepartmentID 
-       ,ExtractOutDepName([Department])                                                 AS DepartmentName 
-       ,ExtractOutCollName([Department])                                                AS CollegeName 
-       ,True                                                                            AS IsStudent into ImportAuthor
+       ,[First Name] & " " & [Last name]                                            AS FullName 
+       ,FixTitle([Position])                                                        AS JobTitle 
+       ,ExtractOutDepID([Department])                                               AS DepartmentID 
+       ,ExtractOutDepName([Department])                                             AS DepartmentName 
+       ,ExtractOutCollName([Department])                                            AS CollegeName 
+       ,True                                                                        AS IsStudent into ImportAuthor
 FROM LinkAuthor
-WHERE ID Not IN ( SELECT distinct Code FROM Author)  
+WHERE IIF(IsNull(ID), ([First Name] & " " & [Last name]) not IN ( SELECT DISTINCT FullName FROM Author) , ID not IN ( SELECT DISTINCT Code FROM Author) )  
